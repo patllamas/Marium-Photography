bc47c78a46dfd86636bfeaf9df509204cda2548b
patllamas <pat.llamas@outlook.com>
2021-07-13 14:08:10 -0400

    added a background and basic CSS for the customer

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,16 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <div className='Hero'>
+      <div className='HeroGroup'>
+        <h1>Zerazal Photography</h1>
+        <p>Speed and Aesthetics</p>
+        <p>Unrivaled Photography and Graphics Design</p>
+        <br />
+        <Link to="/page-2/">Hire Me</Link>
+      </div>
+    </div>
+
   </Layout>
 )
 
